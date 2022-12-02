224a587e49c85bdc8053ca2ff907e7d10a77ffb5
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-12-02 22:26:57 +0300

    create component sidebar

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import { Error, Loader, SongCard } from '../components';
+import { useDispatch, useSelector } from 'react-redux';
 import { genres } from '../assets/constants';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+	const dispatch = useDispatch();
+	const { activeSong, isPlaying } = useSelector((state) => state.player);
 	const { data, isFetching, error } = useGetTopChartsQuery();
 	const genreTitle = 'Pop';
 
-	if (isFetching) return <Loader titel='Loading songs...' />;
+	if (isFetching) return <Loader title='Loading songs...' />;
 	if (error) return <Error />;
 	return (
 		<div className='flex flex-col'>
@@ -27,7 +31,14 @@ const Discover = () => {
 
 			<div className='flex flex-wrap sm:justify-start justify-center gap-8'>
 				{data.map((song, i) => (
-					<SongCard key={song.key} song={song} i={i} />
+					<SongCard
+						key={song.key}
+						song={song}
+						isPlaying={isPlaying}
+						activeSong={activeSong}
+						data={data}
+						i={i}
+					/>
 				))}
 			</div>
 		</div>
